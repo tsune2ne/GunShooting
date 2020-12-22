da596129d62f40526a4c4af837beb1e064de93fd
tsune <std0dc00@gmail.com>
2020-12-23 00:38:07 +0900

    first commit

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GunShooting
+{
+    public class Controller : MonoBehaviour
+    {
+        [SerializeField] Player Player;
+
+        void Start()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Player.Shoot();
+            }
+
+            var rotateX = Input.GetAxis("Mouse X");
+            var rotateY = Input.GetAxis("Mouse Y");
+            if (Mathf.Abs(rotateX + rotateY) > 0)
+            {
+                Player.Rotate(rotateX, rotateY);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GunShooting
+{ 
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] GameObject Hand;
+
+        public void Rotate(float rotateX, float rotateY)
+        {
+            gameObject.transform.Rotate(0, -rotateX, 0);
+            Hand.transform.Rotate(rotateY, 0, 0);
+        }
+
+        public void Shoot()
+        {
+            Debug.Log("Shoot!");
+        }
+    }
+}
